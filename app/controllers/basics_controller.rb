38e65226db64048023a352b9aad0cb44b7adb885
ilovesh <zhj.via@gmail.com>
2012-11-28 08:42:57 -0500

    add recent lists to homepage

--- app/controllers/basics_controller.rb
+++ app/controllers/basics_controller.rb
@@ -5,6 +5,7 @@ class BasicsController < ApplicationController
       @popular_upcoming_courses = Course.upcoming.sort_by{ |c| -c.will_take_users.count }[0..4]
       @popular_finished_courses = Course.finished.sort_by{ |c| -c.taken_users.count }[0..4]
       @discussions = Discussion.all(limit: 5)
+      @lists       = List.all(limit: 5)
     end
   end
 end
