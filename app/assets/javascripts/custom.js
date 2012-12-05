331bfe5d278ae70703cba57acb62899a3f44ec93
Qian JIN <jinqian.hz@gmail.com>
2012-12-05 20:11:44 +0100

    update

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,35 +1,5 @@
 $(document).ready(function(){
-	/* For the usage of test jquery loading 
-	$("#logo").mouseenter(function(){
-		$("a").fadeOut('slow');
+    /* $("#popular_ongoing_courses").mouseenter(function(){
+		$(".section-title").fadeOut('slow');
 	});  */
-
-	var availableTags = [
-		"ActionScript",
-		"AppleScript",
-		"Asp",
-		"BASIC",
-		"C",
-		"C++",
-		"Clojure",
-		"COBOL",
-		"ColdFusion",
-		"Erlang",
-		"Fortran",
-		"Groovy",
-		"Haskell",
-		"Java",
-		"JavaScript",
-		"Lisp",
-		"Perl",
-		"PHP",
-		"Python",
-		"Ruby",
-		"Scala",
-		"Scheme"
-	];
-	
-	$("#coursenames").autocomplete({
-		source: availableTags
-	});
 });
