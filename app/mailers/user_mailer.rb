603ff3c6b6ae8f46d9ccc6b0b98f660f5dbc96d5
ilovesh <zhj.via@gmail.com>
2013-01-12 09:40:01 -0500

    Change mail default from

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "contact@courigin.com"
+  default from: "Courigin<contact@courigin.com>"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
