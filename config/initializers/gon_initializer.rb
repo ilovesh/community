4316f51e0b72b86a895ed085fde26f8587e558f9
ilovesh <zhj.via@gmail.com>
2013-01-14 15:52:32 -0500

    Add Gon initializer back

--- config/initializers/gon_initializer.rb
+++ config/initializers/gon_initializer.rb
@@ -1,3 +1,3 @@
 # global variable for search form autocomplete
-#Gon.global.courses = Course.all.map(&:full_name)
-#Gon.global.courses_with_path = Course.all.to_json(only:[:id], methods:[:full_name, :course_path])
+Gon.global.courses = Course.all.map(&:full_name)
+Gon.global.courses_with_path = Course.all.to_json(only:[:id], methods:[:full_name, :course_path])
