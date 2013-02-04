9dcaae79d25172b1aa7c788645f71a352b9a0272
zhj <zhj.via@gmail.com>
2013-02-03 23:06:12 -0600

    Fix omniauth.rb bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
   configure do |config|
     config.path_prefix = '/auth'
   end
